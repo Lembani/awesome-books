53fe4cee906548605180964606b1c51727b03eb5
Lembani Sakala <lembanisakala@gmail.com>
2022-04-19 18:08:14 +0200

    Create Script to select DOM elements

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const { bookForm } = document.forms;
+const { title } = bookForm;
+const { author } = bookForm;
+const addBtn = document.getElementById('addBtn');
+const booksContainer = document.querySelector('.booksContainer');
