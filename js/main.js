4f70a3e5661620a4fb87cb9512f644c76983ce24
Lembani Sakala <lembanisakala@gmail.com>
2022-04-19 18:21:00 +0200

    Update script with: - books collection(Array and Object) - addBook function to add a book to localStorage - addButton invokes the addBook function on click - etc

--- js/main.js
+++ js/main.js
@@ -2,4 +2,27 @@ const { bookForm } = document.forms;
 const { title } = bookForm;
 const { author } = bookForm;
 const addBtn = document.getElementById('addBtn');
-const booksContainer = document.querySelector('.booksContainer');
+// const booksContainer = document.querySelector('.booksContainer');
+
+let books = [];
+books = JSON.parse(localStorage.getItem('books')) !== null ? (books = JSON.parse(localStorage.getItem('books'))) : [];
+
+const addBook = () => {
+  books = JSON.parse(localStorage.getItem('books')) !== null ? (books = JSON.parse(localStorage.getItem('books'))) : [];
+
+  const book = {
+    id: 0,
+    title: '',
+    author: '',
+  };
+  book.title = title.value;
+  book.author = author.value;
+  book.id = books.length + 1;
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
+  title.value = '';
+  author.value = '';
+  // call showBooks() here..
+};
+
+addBtn.addEventListener('click', addBook);
